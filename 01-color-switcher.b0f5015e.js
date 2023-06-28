73accbd8221c0b0f58f0f60ca6dd9d411993f387
aborzym <borzym.a@gmail.com>
2023-06-28 22:20:31 +0000

    Deploying to gh-pages from @ aborzym/goit-js-hw-09@ad5b5f98bc983d80cc640eb0a3632eee57546c7a 🚀

---
+++ 01-color-switcher.b0f5015e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let r=null;t.addEventListener("click",(()=>{r=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.style.backgroundColor=t}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.b0f5015e.js.map
