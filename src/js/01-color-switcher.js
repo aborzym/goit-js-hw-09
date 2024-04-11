25c5c733ff4077f8cca4faec95811f9b496f1720
aborzym <borzym.a@gmail.com>
2024-04-11 13:41:17 +0200

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let interval;
+
+startBtn.addEventListener('click', _ev => {
+  interval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', 'disabled');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(interval);
+  startBtn.removeAttribute('disabled');
+});
